d635b63b1c41ac78a486a22b2f340b5b81a0f7eb
Drebakare <bakaredmlr@gmail.com>
2022-03-14 23:34:36 +0100

    creates project theme

---
+++ src/theme/components/Button.ts
@@ -0,0 +1,15 @@
+export const Button = {
+    variants: {
+      action: {
+        _hover: {
+          color: '#fff',
+          bg: "primary"
+        }, 
+        color: '#fff',
+        bg: "primary"       
+        },
+
+  }
+}
+  
+  
