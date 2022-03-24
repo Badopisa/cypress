cf7feeceeacaff97f8ca2009d6c9f997020564d6
Drebakare <bakaredmlr@gmail.com>
2022-03-24 02:25:06 +0100

    sets up tdd process and structure

---
+++ src/test/pages/test-sample.test.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import {render, screen} from "../text-utils";
+import TextSample from "../../pages/test-sample";
+
+describe("HomePage", () => {
+  it("should render the heading", () => {
+    render(<TextSample />);
+
+    const heading = screen.getByText(
+      /Testing Next.js With Jest and React Testing Library/i
+    );
+
+    // we can only use toBeInTheDocument because it was imported
+    // in the jest.setup.js and configured in jest.config.js
+    expect(heading).toBeInTheDocument();
+  });
+});
