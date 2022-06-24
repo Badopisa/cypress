05cb3caafa2a6188e5ca440aab3b938969ef1105
Nnolum Esther <esthernnolum@gmail.com>
2022-06-24 08:33:33 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "api_token" {
+  default = "dop_v1_7855b59c48273a57787c5d48a91b1e697afbc2f25104b35adc6bcbc60b7c180c"
+}
+
+variable "region" {
+  default = "LON1"
+}
+
+variable "ssh_fingerprint" {
+  default =  "a9:a5:20:f5:ea:70:32:5f:2c:08:4e:6d:66:65:f1:f6"
+}
+
+variable "number_servers" {
+  default = "1"
+}
+
+variable "access_id" {
+  default =  "RYDGWWQFJIEDH6DTEQVY"
+}
+
+variable "secret_key" {
+  default = "X9phlUou2z1Zm45DU1GqaQsdjRwSaWVU47ZzDAcgsJo"
+}
