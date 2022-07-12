fd9a66e48ef1060f477dd0e8db758bca0b5d5947
Nnolum Esther <esthernnolum@gmail.com>
2022-07-12 08:15:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,25 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "Parallelscore"
-
-    workspaces {
-      name = "parallelscore_work"
-    }
+  backend "s3" {
+  endpoint = "https://nyc3.digitaloceanspaces.com"
+  region = "us-east-1"
+  bucket = "sonalysis-media-space" // name of your space
+  key = "sonalysistfstate/terraform.tfstate"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
   }
-  
+
   required_providers {
-  digitalocean = {
-    source = "digitalocean/digitalocean"
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.6.0"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
   }
 }  
@@ -18,14 +27,15 @@ provider "digitalocean" {
     #token = var.api_token
 }
 
-resource "digitalocean_droplet" "sonalysis" {
-   count = 1
-   name = "sonalysis-${count.index}"
-   image = "ubuntu-20-04-x64"
-   size = "s-1vcpu-1gb"
-   region = "LON1"
-   ssh_keys = [
-        var.ssh_fingerprint
-   ]
-   
+#create Kubernetes deployment
+resource "digitalocean_kubernetes_cluster" "sonalysis-k8s-1-22-8-do" {
+  name   = "sonalysis-k8s-1-22-8-do"
+  region = "nyc3"
+  version = "1.22.8-do.1"
+
+  node_pool {
+    name       = "pool-sonalysis"
+    size       = "s-2vcpu-4gb-intel"
+    node_count = 2
+  }
 }
